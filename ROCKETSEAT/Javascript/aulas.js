8ccf68d193d84b5742fff869c56041b6bef3a8a7
joaoarruda2297 <jovitorarruda@hotmail.com>
2023-09-28 15:48:15 -0300

    teste

---
+++ ROCKETSEAT/Javascript/aulas.js
@@ -0,0 +1,24 @@
+//declaração de tipos
+let chama = "Qualquer coisa"
+chama = 2
+chama = 3.5
+
+//constande nao pode ser mudada
+const c = 2
+const d = "chama"
+
+//funcoes
+let x = "olá"
+function digaOla(x)
+{
+    return x
+}
+
+//objeto
+const celular = {
+    cor: "preto",
+    numero: 11973060511,
+    ligar: function() {
+        alert("ligando")
+    }
+}
