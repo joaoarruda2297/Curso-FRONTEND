5073f3d2c92c21831c7ba38e38180aecc7ff8d81
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-01-08 21:23:15 -0300

    Projetinho de zuera

---
+++ PROJETINHO/main.js
@@ -0,0 +1,23 @@
+const botao = document.querySelector('#bot2');
+botao.addEventListener('mouseover', changePosition);
+function changePosition() {
+    
+    let x = Math.random() * 100;
+    let y = Math.random() * 100;
+
+    console.log(x,y);
+
+    if(x > 95){
+        botao.style.left = `calc(${x}vw - 100px)`;
+    }
+    else{
+        botao.style.left = `${x}vw`;
+    }
+
+    if(y > 95){
+        botao.style.top = `calc(${y}vh - 20px)`;
+    }
+    else{
+        botao.style.top = `${y}vh`;
+    }
+}
