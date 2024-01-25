1475c9bcdd7c96eb49ba995e9d4fdfc74655eb7e
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-01-25 19:55:40 -0300

    .

--- ROCKETSEAT/Fase07/API/src/server.js
+++ ROCKETSEAT/Fase07/API/src/server.js
@@ -11,5 +11,12 @@ app.get("/message/:id/:user", (request, response) => {
     );
 });
 
+app.get("/users", (request,response) => {
+    const {test1, test2} = request.query;
+    response.send(`Pagina: ${test1}. Mostrar: ${test2}`);
+});
+//para acessar esse é necessário colocar da forma:
+//localhost:3333/users?test1=X&test2=Y
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
