abe072c02655f80e8d0d4cdc91c94551df85d986
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-01-24 21:31:14 -0300

    Básico de Node.js - Route/Query Params (GET)

---
+++ ROCKETSEAT/Fase07/API/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/message/:id/:user", (request, response) => {
+    const {id, user} = request.params;
+    
+    response.send(`
+    O id da mensagem é: ${id}.
+    Para o usuário: ${user}.`
+    );
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
