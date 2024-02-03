7d1ed3c15ae11d307ad3863ce790cf4a8749237a
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-02-02 21:18:12 -0300

    Atualização de migrations por KNEX

--- ROCKETSEAT/Fase07/API/knexfile.js
+++ ROCKETSEAT/Fase07/API/knexfile.js
@@ -1,9 +1,14 @@
+const path = require("path");
+
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: Path2D(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    migrations: {
+      directory: path.resolve(__dirname,"src","database", "knex", "migrations")
     },
     useNullAsDefault: true
-  }
+  },
 };
