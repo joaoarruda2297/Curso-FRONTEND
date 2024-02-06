24990faa9b8403d84342ce7ac49222c17a5832a3
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-02-05 21:13:58 -0300

    Funcionamento Básico de REACT

--- ROCKETSEAT/Fase08/rocketnotes/src/pages/Details/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/pages/Details/index.jsx
@@ -1,12 +1,16 @@
 import {Container} from "./styles";
 
+import { Button } from "../../components/Button";
+
 export function Details() {
 
   return (
     <Container>
       <h1>Hello World</h1>
 
-      <div>Teste 1 2 3</div>
+      <Button title="Entrar" loading/>
+      <Button title="Cadastrar" />
+      <Button title="Voltar"/>
     </Container>
 
   )
