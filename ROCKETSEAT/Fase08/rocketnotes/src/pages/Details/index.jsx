02dcf2bd1f363be5541302368571859633f50992
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-02-22 20:15:37 -0300

    Estruturação da Note Preview

--- ROCKETSEAT/Fase08/rocketnotes/src/pages/Details/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,49 @@
-import {Container} from "./styles";
+import {Container, Links, Content} from "./styles";
 import {Header} from "../../components/Header";
-import { Button } from "../../components/Button";
+import {Button} from "../../components/Button";
+import {Section} from "../../components/Section";
+import {Tag} from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
 
   return (
     <Container>
-      <Header></Header>
-      <Button title="Voltar"/>
+      <Header/>
+
+      <main>
+        <Content>
+          <ButtonText title="Hehehe"/>
+
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">Link 1</a>
+              </li>
+              <li>
+                <a href="#">Link 2</a>
+              </li>
+              <li>
+                <a href="#">Link 3</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="vagabundo"></Tag>
+            <Tag title="chama"></Tag>
+          </Section>
+
+          <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
 
   )
