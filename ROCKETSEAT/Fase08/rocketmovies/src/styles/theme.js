37ef6a282f09f684e9af78a382b336e7fc66dbe3
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-02-27 17:42:10 -0300

    Theme colors for the project

---
+++ ROCKETSEAT/Fase08/rocketmovies/src/styles/theme.js
@@ -0,0 +1,17 @@
+export default {
+    COLORS: {
+      BACKGROUND_900: "#1C1B1E",
+      BACKGROUND_800: "#282124",
+      BACKGROUND_700: "#ff859b0d",
+  
+      WHITE_100: "#F4EDE8",
+      WHITE_200: "#E5E5E5",
+      BLACK: "#312E38",
+      PINK: "#FF859B",
+  
+      GRAY_100: "#948F99",
+      GRAY_300: "#262529",
+  
+      STROKE: "#3E3B47"
+    }
+  }
