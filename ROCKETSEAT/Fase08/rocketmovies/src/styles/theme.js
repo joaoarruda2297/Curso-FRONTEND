79c60334d281746abc3c3761652b4f9bc12a32c1
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-02-28 20:11:53 -0300

    Adding new colors

--- ROCKETSEAT/Fase08/rocketmovies/src/styles/theme.js
+++ ROCKETSEAT/Fase08/rocketmovies/src/styles/theme.js
@@ -3,10 +3,14 @@ export default {
       BACKGROUND_900: "#1C1B1E",
       BACKGROUND_800: "#282124",
       BACKGROUND_700: "#ff859b0d",
-  
+      
+      WHITE: "#FFFFFF",
       WHITE_100: "#F4EDE8",
       WHITE_200: "#E5E5E5",
+      WHITE_300: "#999591",
+      WHITE_400: "#CAC4CF",
       BLACK: "#312E38",
+      BLACK_100: "#0D0C0F",
       PINK: "#FF859B",
   
       GRAY_100: "#948F99",
