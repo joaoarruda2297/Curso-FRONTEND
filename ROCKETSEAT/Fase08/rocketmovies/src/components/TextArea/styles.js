597e98dc3c3f0633f9f01098edd96113d10eb81d
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-02-28 20:14:28 -0300

    TexteArea component complete

---
+++ ROCKETSEAT/Fase08/rocketmovies/src/components/TextArea/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.textarea`
+    background-color: ${({theme}) => theme.COLORS.GRAY_300};
+    border: none;
+    border-radius: 10px;
+    width: 100%;
+    height: 274px;
+    resize: none;
+    padding: 19px 16px;
+    color: ${({theme}) => theme.COLORS.WHITE_100};
+
+    &::placeholder{
+        color: ${({theme}) => theme.COLORS.GRAY_100};
+    }
+`;
