2077992f664d57e2c5c318c6a66e3fd7309951ea
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-01 11:49:27 -0300

    UserImg Component

--- ROCKETSEAT/Fase08/rocketmovies/src/components/UserImg/index.jsx
+++ ROCKETSEAT/Fase08/rocketmovies/src/components/UserImg/index.jsx
@@ -1,6 +1,6 @@
 import { Container } from "./styles";
 
-export function UserImg(){
+export function UserImg({size=64}){
     return(
         <Container>
             <img src="https://github.com/joaoarruda2297.png" alt="User image" />
--- ROCKETSEAT/Fase08/rocketmovies/src/components/UserImg/styles.js
+++ ROCKETSEAT/Fase08/rocketmovies/src/components/UserImg/styles.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
-
+    > img{
+        width: ${({size}) => `${size ?? 64}px`};
+        height: ${({size}) => `${size ?? 64}px`};
+        border-radius: 50%;
+        border: 1px solid ${({theme}) => theme.COLORS.STROKE};
+    }
 `;
