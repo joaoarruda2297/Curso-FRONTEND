f45d1d3ac7bfb790ea896a55b0f5ca435f3162e4
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-01 12:01:27 -0300

    Adding ternary for undefined

--- ROCKETSEAT/Fase08/rocketmovies/src/components/UserImg/styles.js
+++ ROCKETSEAT/Fase08/rocketmovies/src/components/UserImg/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     > img{
-        width: ${({size}) => `${size}px`};
-        height: ${({size}) => `${size}px`};
+        width: ${({size}) => `${size ?? 64}px`};
+        height: ${({size}) => `${size ?? 64}px`};
         border-radius: 50%;
         border: 1px solid ${({theme}) => theme.COLORS.STROKE};
     }
