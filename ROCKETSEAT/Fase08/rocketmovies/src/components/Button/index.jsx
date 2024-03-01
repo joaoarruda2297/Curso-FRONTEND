925f148f49d9df8e89ca587173f6b5048f2a9571
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-01 13:21:20 -0300

    Update children prop

--- ROCKETSEAT/Fase08/rocketmovies/src/components/Button/index.jsx
+++ ROCKETSEAT/Fase08/rocketmovies/src/components/Button/index.jsx
@@ -1,12 +1,13 @@
 import { Container } from "./styles";
 
-export function Button({title, loading = false, ...rest}){
+export function Button({title, children, loading = false, ...rest}){
     return(
     <Container 
         type="button"
         disabled={loading}//isso aqui faz dizer que disabled é true
         {...rest}//isso aqui é pra puxar todas as outras propriedades
     >
+        {children ?? ""}
         {loading ? 'Carregando...' : title}
     </Container>
     );
