3ceb3834babb453f83e1ddfae50b186b87785f3c
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-01 20:28:06 -0300

    Page Home

---
+++ ROCKETSEAT/Fase08/rocketmovies/src/pages/Home/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-rows: 116px auto;
+    grid-template-areas: 
+    "header"
+    "content";
+
+    > main{
+        grid-area: content;
+        padding: 40px 123px;
+        max-width: 1100px;
+
+        justify-self: center;
+
+        > #little-header{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+
+            > Button{
+                width: 207px;
+            }
+        }
+    }
+`;
+
+
+export const Content = styled.div`
+    overflow-y: auto;
+    margin: 40px auto 0;
+    height: 600px;
+    padding: 0 16px 0 0;
+
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`;
