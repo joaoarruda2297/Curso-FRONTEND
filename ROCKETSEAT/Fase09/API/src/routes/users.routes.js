3bfd0be83fa2344a075e50ee4d01db21d20b2a6a
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-04 16:39:56 -0300

    Forgot SRC paste

---
+++ ROCKETSEAT/Fase09/API/src/routes/users.routes.js
@@ -0,0 +1,11 @@
+const {Router} = require("express");
+const UsersController = require("../controllers/UsersController");
+
+const userRoutes = Router();
+
+const usersController = new UsersController();
+
+userRoutes.post("/", myMiddleware, usersController.create);
+userRoutes.put("/:id", usersController.update);
+
+module.exports = userRoutes;
