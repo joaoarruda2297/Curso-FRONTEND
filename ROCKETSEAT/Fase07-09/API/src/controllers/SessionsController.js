8492ade6ff9ea0d2efe9defa6c26d8bebadee969
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-06 21:36:17 -0300

    Validation for user and password

--- ROCKETSEAT/Fase07-09/API/src/controllers/SessionsController.js
+++ ROCKETSEAT/Fase07-09/API/src/controllers/SessionsController.js
@@ -1,6 +1,32 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
 class SessionsController{
-    async create(){
+    async create(request, response){
+        const { email,password } = request.body;
         
+        const user = await knex("users").where({email}).first();
+
+        if(!user){//nao vale a pena verificar os dois juntos pois se nao houver usuário igual já tem que parar
+            throw new AppError("E-mail inválido", 401);
+        }
+        
+        const passwordMatched = await compare(password, user.password);//devolve um boolean
+
+        if(!passwordMatched){
+            throw new AppError("Senha inválida", 401);
+        }
+                
+        const { secret, expiresIn } = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        });
+
+        return response.json({user, token});
     }
 }
 
