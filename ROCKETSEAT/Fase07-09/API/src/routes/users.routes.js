4f35dccd799f4470df0a83a60cf5e6923d15b57b
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-11 19:43:18 -0300

    ...

--- ROCKETSEAT/Fase07-09/API/src/routes/users.routes.js
+++ ROCKETSEAT/Fase07-09/API/src/routes/users.routes.js
@@ -1,12 +1,20 @@
 const {Router} = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const userRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 userRoutes.post("/", usersController.create);
 userRoutes.put("/", ensureAuthenticated, usersController.update);
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+    console.log(req.file.filename);
+    response.json();
+});
 
 module.exports = userRoutes;
