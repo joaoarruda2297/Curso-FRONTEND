e4aa9eb01d03048b72608bfa376687ceee9c08a2
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-14 20:00:22 -0300

    sending data to backend

--- ROCKETSEAT/Fase08/rocketnotes/src/pages/SignIn/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Link } from 'react-router-dom';
@@ -5,7 +6,17 @@ import { FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Form, Background } from "./styles";
 
+import { useAuth } from '../../hooks/auth'
+
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { signIn } = useAuth();
+    
+    function handleSignIn(){
+        signIn({email, password});
+    }
+
     return(
         <Container>
             <Form>
@@ -17,16 +28,18 @@ export function SignIn(){
                 <Input
                     placeholder = "E-mail"
                     type = "text"
-                    icon = {FiMail}    
+                    icon = {FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder = "Senha"
                     type = "password"
-                    icon = {FiLock}    
+                    icon = {FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar"/>
+                <Button title="Entrar" onClick={handleSignIn}/>
 
                 <Link to="/register">
                     Criar conta
