b3ddc0c1fb71a6dc5d000a8f8449608e8f9c5518
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-18 18:13:28 -0300

    Adding SignOut function and effect for render

--- ROCKETSEAT/Fase08/rocketnotes/src/hooks/auth.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
 
 export const AuthContext = createContext({});
@@ -10,6 +10,9 @@ function AuthProvider({children}){
         try{
             const response = await api.post("/sessions", {email,password});
             const {user,token} = response.data;
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
 
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({user, token});
@@ -23,8 +26,32 @@ function AuthProvider({children}){
         }
     }
 
+    function signOut(){
+        const token = localStorage.removeItem("@rocketnotes:token");
+        const user = localStorage.removeItem("@rocketnotes:user");
+
+        setData({});
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({
+                user: JSON.parse(user), 
+                token
+            });
+        }
+    }, [])
+
     return(
-        <AuthContext.Provider value={{ signIn, user: data.user }}>
+        <AuthContext.Provider value={{ 
+            signIn,
+            signOut,
+            user: data.user 
+        }}>
             {children}
         </AuthContext.Provider>
     );
