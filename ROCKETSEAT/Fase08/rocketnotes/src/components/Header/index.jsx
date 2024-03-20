17eff17381e153a71aa5100ad2d720606309e8bf
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-19 21:44:30 -0300

    Changing the component to use the right image

--- ROCKETSEAT/Fase08/rocketnotes/src/components/Header/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/components/Header/index.jsx
@@ -1,17 +1,21 @@
 import { RiShutDownLine } from 'react-icons/ri';
 import {Container, Profile, Logout} from "./styles";
 import { useAuth } from '../../hooks/auth';
+import { api } from '../../services/api';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
 export function Header(){
-    const {signOut} = useAuth();
+    const {signOut, user} = useAuth();
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
     return(
         <Container>
             <Profile to="/profile">
-                <img src="https://github.com/joaoarruda2297.png" alt="Imagem do Github (do usuário)" />
+                <img src={avatarUrl} alt={user.name} />
                 <div>
                     <span>Bem vindo,</span>
-                    <strong>João Arruda</strong>
+                    <strong>{user.name}</strong>
                 </div>
             </Profile>
 
