803c266b8b921ccde339178feb6b7b157fc74c63
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-03-27 18:23:12 -0300

    complete server.js

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/server.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/server.js
@@ -1,7 +1,33 @@
+require("express-async-errors");
 const express = require("express");
 
+const database = require("./database/sqlite");
+const routes = require("./routes");
+const AppError = require("./utils/AppError");
+
 const app = express();
+app.use(express.json());//dizendo a forma que vai receber as coisas
+
+app.use(routes);
+
+database();
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 const PORT = 2297;
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
