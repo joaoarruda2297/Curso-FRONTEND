33ab83abefd6f92a0fef6177165996296ba83e96
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-02 19:15:20 -0300

    Created method for index

---
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/controllers/MovieTagsController.js
@@ -0,0 +1,14 @@
+const knex = require("../database/knex");
+
+class MovieTagsController{
+    async index(req,res){
+        const {user_id} = req.params;
+
+        const tags = await knex("movie_tags")
+            .where({user_id})
+        
+        return res.json(tags);
+    }
+}
+
+module.exports = MovieTagsController;
