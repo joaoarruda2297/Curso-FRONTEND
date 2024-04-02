d9c7d7f569e3a02f276aaed54251545fac84c8ee
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-02 19:16:05 -0300

    adding notes and tags routes

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/routes/index.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/routes/index.js
@@ -1,9 +1,13 @@
 const {Router} = require("express");
 
 const usersRouter = require("./user.routes");
+const moviesNotesRoutes = require("./moviesNotes.routes");
+const moviesTagsRoutes = require("./moviesTags.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/moviesnotes", moviesNotesRoutes);
+routes.use("/moviestags", moviesTagsRoutes);
 
 module.exports = routes;
