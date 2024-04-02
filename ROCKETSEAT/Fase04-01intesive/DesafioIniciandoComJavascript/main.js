3a22c4acf37cf6cd82776af6f95778e8caa2cb93
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-02 19:37:57 -0300

    Completing challenges for intensive phase

---
+++ ROCKETSEAT/Fase04-01intesive/DesafioIniciandoComJavascript/main.js
@@ -0,0 +1,62 @@
+function hello(){//1
+    console.log("Hello World!");
+}
+
+function soma(){//2
+    let a = 1;
+    let b = 2;
+    console.log(a+b);
+}
+
+function type(){//3
+    let x = "Palavra";
+    let res = (typeof(x) === Number) ? "É um número" : "Não é um número";
+    console.log(res);
+}
+
+function type2(){//4
+    let x = "Palavra";
+    let res = (typeof(x) === String) ? "É uma string" : "Não é uma string";
+    console.log(res);
+}
+
+function type3(){//5
+    let x = "Palavra";
+    let res = (typeof(x) === Boolean) ? "É um boolean" : "Não é um boolean";
+    console.log(res);
+}
+
+function sub(){//6
+    let a = 1;
+    let b = 2;
+    console.log(a-b);
+}
+
+function mult(){//7
+    let a = 1;
+    let b = 2;
+    console.log(a*b);
+}
+
+function div(){//8
+    let a = 1;
+    let b = 2;
+    console.log(a/b);
+}
+
+function par(){//9
+    let x = 8;
+    let res = (x%2 == 0) ? "É par" : "Não é par";
+    console.log(res);
+}
+
+function impar(){//10
+    let x = 8;
+    let res = (x%2 == 0) ? "Não é ímpar" : "É ímpar";
+    console.log(res);
+}
+
+
+
+
+
