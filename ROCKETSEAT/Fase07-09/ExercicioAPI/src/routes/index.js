9ccfe57d2deb19ade6ef90f6777f9d15a7cc3f09
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-03 20:37:31 -0300

    Adding session routes

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/routes/index.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/routes/index.js
@@ -1,13 +1,15 @@
 const {Router} = require("express");
 
 const usersRouter = require("./user.routes");
-const moviesNotesRoutes = require("./moviesNotes.routes");
-const moviesTagsRoutes = require("./moviesTags.routes");
+const moviesNotesRouter = require("./moviesNotes.routes");
+const moviesTagsRouter = require("./moviesTags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
-routes.use("/moviesnotes", moviesNotesRoutes);
-routes.use("/moviestags", moviesTagsRoutes);
+routes.use("/moviesnotes", moviesNotesRouter);
+routes.use("/moviestags", moviesTagsRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;
