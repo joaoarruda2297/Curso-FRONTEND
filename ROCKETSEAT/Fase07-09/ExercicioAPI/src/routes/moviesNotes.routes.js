9d57c4a61d77fc926f807134ac58b9f40e108803
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-03 20:35:34 -0300

    Adding middleware in the process

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/routes/moviesNotes.routes.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/routes/moviesNotes.routes.js
@@ -1,10 +1,13 @@
 const {Router} = require("express");
 const MoviesNotesController = require("../controllers/MoviesNotesController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const moviesNotesController = new MoviesNotesController();
 const movieNotesRoutes = Router();
 
-movieNotesRoutes.post("/:user_id", moviesNotesController.create);
+movieNotesRoutes.use(ensureAuthenticated);
+
+movieNotesRoutes.post("/", moviesNotesController.create);
 movieNotesRoutes.get("/:id", moviesNotesController.show);
 movieNotesRoutes.delete("/:id", moviesNotesController.delete);
 movieNotesRoutes.get("/", moviesNotesController.index);
