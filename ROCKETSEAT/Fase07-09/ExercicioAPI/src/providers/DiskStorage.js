83f25bee5eb3cac1df9bee5af6c1b8667bf68969
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-09 20:20:03 -0300

    deleteFile function for diskStorage

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/providers/DiskStorage.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/providers/DiskStorage.js
@@ -11,6 +11,18 @@ class DiskStorage{
 
         return file;
     }
+
+    async deleteFile(file){
+        const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
+
+        try{
+            await fs.promises.stat(filePath);
+        } catch{
+            return;
+        }
+
+        await fs.promises.unlink(filePath);
+    }
 }
 
 module.exports = DiskStorage;
