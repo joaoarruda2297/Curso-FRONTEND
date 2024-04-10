2f16325e28930ec0f5a2c285c2d1343ec0764078
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-10 10:54:42 -0300

    Adding comments

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/providers/DiskStorage.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/providers/DiskStorage.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
+const fs = require("fs");//FS é de FILE SYSTEM!!!
 const path = require("path");
 const uploadConfig = require("../configs/upload");
 
 class DiskStorage{
     async saveFile(file){
-        await fs.promises.rename(
+        await fs.promises.rename(//rename nao é renomear, é mudar de lugar fazendo rename do path
             path.resolve(uploadConfig.TMP_FOLDER, file),
             path.resolve(uploadConfig.UPLOADS_FOLDER, file)
         );
@@ -16,12 +16,12 @@ class DiskStorage{
         const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
 
         try{
-            await fs.promises.stat(filePath);
+            await fs.promises.stat(filePath);//stat devolve o objeto com o arquivo que está sendo buscado
         } catch{
-            return;
+            return; //nao precisamos de instancia de erro, pois se nao encontrar, o arquivo nao precisa ser deletado
         }
 
-        await fs.promises.unlink(filePath);
+        await fs.promises.unlink(filePath);//unlink serve para deletar
     }
 }
 
