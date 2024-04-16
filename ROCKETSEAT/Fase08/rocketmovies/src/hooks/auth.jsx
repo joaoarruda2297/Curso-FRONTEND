6e46ba6bffe8918fc00f8e1e4098ab99c984f1a2
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-16 20:17:22 -0300

    Adding data on localstorage

--- ROCKETSEAT/Fase08/rocketmovies/src/hooks/auth.jsx
+++ ROCKETSEAT/Fase08/rocketmovies/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
 
 export const AuthContext = createContext({});
@@ -11,8 +11,11 @@ function AuthProvider({children}){
             const response = await api.post("/sessions", {email, password});
             const {user, token} = response.data;
 
+            localStorage.setItem("@rocketmovies:user", JSON.stringify(user));
+            localStorage.setItem("@rocketmovies:token", token);
+
             api.defaults.headers.authorization = `Bearer ${token}`;
-            setData({user, token});
+            setData({user, token});//guardando no state
         }
         catch(error){
             if(error.response){
@@ -24,6 +27,19 @@ function AuthProvider({children}){
         }
 
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketmovies:token");
+        const user = localStorage.getItem("@rocketmovies:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    }, []);
 
     return(
         <AuthContext.Provider value={{signIn, user: data.user}}>
