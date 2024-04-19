85fb15424c19a1d7b797819cd9ae61391a9cf565
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-19 02:02:57 -0300

    Features for exclude button, addMarker and deleteMarker

--- ROCKETSEAT/Fase08/rocketmovies/src/pages/CreateMovie/index.jsx
+++ ROCKETSEAT/Fase08/rocketmovies/src/pages/CreateMovie/index.jsx
@@ -6,7 +6,35 @@ import { TextArea } from "../../components/TextArea";
 import { Marker } from "../../components/Marker";
 import { Button } from "../../components/Button";
 
+import { useState } from "react";
+
 export function CreateMovie(){
+    const [marker,setMarker] = useState([]);
+    const [newMarker, setNewMarker] = useState("");
+
+    const [title,setTitle] = useState("");
+    const [rating, setRating] = useState("");
+    const [comment, setComment] = useState("");
+    
+
+    function handleAddMarker(){
+        setMarker(prevState => [...prevState, newMarker]);
+        setNewMarker("");
+    }
+    function handleDeleteMarker(indexDelete){
+        setMarker(prevState => prevState.filter((_,index) => index !== indexDelete));
+    }
+    function handleSubmitMovie(){
+
+    }
+    function handleDeleteMovie(){
+        setMarker([]);
+        setNewMarker("");
+        setTitle("");
+        setRating("");
+        setComment("");
+    }
+
     return(
         <Container>
             <Header/>
@@ -17,34 +45,59 @@ export function CreateMovie(){
                     <h1>Novo filme</h1>
                     <div className="twoInputs">
                         <Input
+                            value={title}
                             placeholder="Título"
                             type="text"
+                            onChange={e=> setTitle(e.target.value)}
                         />
 
                         <Input
+                            value={rating}
                             placeholder="Sua nota (de 0 a 5)"
                             type="text"
+                            onChange={e=> setRating(e.target.value)}
                         />
                     </div>
 
-                    <TextArea placeholder="Observações"/>
+                    <TextArea
+                        value={comment}
+                        placeholder="Observações" 
+                        onChange={e=> setComment(e.target.value)}
+                    />
 
                     <h2>Marcadores</h2>
 
                     <div className="boxMarker">
+                        {
+                            marker.map((mark, index) => (
+                                <Marker
+                                    key={String(index)}
+                                    size={mark.length}
+                                    value={mark}
+                                    onClick={() => handleDeleteMarker(index)}
+                                />
+                            ))
+                        }
                         <Marker
-                            value="React"
-                        />
-                        <Marker
-                            isNew //precisa ser colocado antes!!!
+                            isNew
                             placeholder="Novo marcador"
+                            value={newMarker}
+                            onChange={e => setNewMarker(e.target.value)}
+                            onClick={handleAddMarker}
                         />
                     </div>
 
                     <div className="boxButton">
-                        <Button title="Excluir filme" className="delete-button"/>
+                        <Button 
+                            title="Excluir filme" 
+                            className="delete-button"
+                            onClick={handleDeleteMovie}
+                        />
 
-                        <Button title="Salvar alterações"/>
+                        <Button 
+                            title="Salvar alterações"
+                            onClick={handleSubmitMovie}
+                        />
                     </div>
 
                 </Content>
