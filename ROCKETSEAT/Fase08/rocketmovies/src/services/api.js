4ab1e86c766e81994ac5a6186859c81364901526
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-19 02:04:03 -0300

    Fix for secutiry on token

--- ROCKETSEAT/Fase08/rocketmovies/src/services/api.js
+++ ROCKETSEAT/Fase08/rocketmovies/src/services/api.js
@@ -3,3 +3,21 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: "http://localhost:2297"
 });
+
+api.interceptors.response.use((response) => response, (error) => {
+    if(error.response?.status === 401){
+        window.dispatchEvent(new Event('sessionExpired'));
+        window.location.href = `/login?callbackUrl=${encodeURIComponent(window.location.pathname)}`;
+    }
+
+    return Promise.reject(error);
+})
+
+api.interceptors.request.use((request) => {
+    const token = localStorage.getItem("@rocketmovies:token");
+
+    if(token)
+        request.headers.Authorization = `Bearer ${token}`;
+
+    return request
+})
