6423b1811929a139d3451ae18c32311a4d9ff948
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-19 02:08:02 -0300

    Feature to go back to the page client was asking when he couldnt acess by not authenticated

--- ROCKETSEAT/Fase08/rocketmovies/src/pages/SignIn/index.jsx
+++ ROCKETSEAT/Fase08/rocketmovies/src/pages/SignIn/index.jsx
@@ -2,7 +2,7 @@ import { Container, Form, Background } from "./styles"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button";
 import { FiMail, FiLock } from 'react-icons/fi';
-import {Link} from "react-router-dom";
+import {Link, useSearchParams, useNavigate} from "react-router-dom";
 
 import { useState } from "react";
 import { useAuth } from "../../hooks/auth";
@@ -10,11 +10,15 @@ import { useAuth } from "../../hooks/auth";
 export function SignIn(){
     const {signIn} = useAuth();
 
+    const navigate = useNavigate();
+    const [params] = useSearchParams();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    function handleSignIn(){
-        signIn({email, password});
+    async function handleSignIn(){
+        await signIn({email, password});
+        navigate(params.get('callbackUrl') ?? '/')
     }
     return(
         <Container>
