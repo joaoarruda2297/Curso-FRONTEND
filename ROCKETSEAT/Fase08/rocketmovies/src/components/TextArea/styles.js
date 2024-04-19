54a24c1853116a125304fc8a833164da41a4df98
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-18 23:54:40 -0300

    Changing textarea

--- ROCKETSEAT/Fase08/rocketmovies/src/components/TextArea/styles.js
+++ ROCKETSEAT/Fase08/rocketmovies/src/components/TextArea/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.textarea`
+export const TextArea = styled.textarea`
     background-color: ${({theme}) => theme.COLORS.GRAY_300};
     border: none;
     border-radius: 10px;
