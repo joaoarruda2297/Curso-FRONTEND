e073ee13a0adbb24b8248d3a8e4a5e1d8a642dc1
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-22 18:59:23 -0300

    Page home complete

--- ROCKETSEAT/Fase08/rocketnotes/src/pages/Home/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/pages/Home/index.jsx
@@ -7,8 +7,57 @@ import { Input } from '../../components/Input';
 import { Note } from '../../components/Note';
 import { Section } from '../../components/Section';
 import { ButtonText } from "../../components/ButtonText";
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+
+import { api } from '../../services/api';
 
 export function Home(){
+    const [tags, setTags] = useState([]);
+    const [tagsSelected, setTagsSelected] = useState([]);
+    const [search, setSearch] = useState("");
+    const [notes, setNotes] = useState([]);
+
+    const navigate = useNavigate();
+
+    function handleTagSelected(tagName){
+        if(tagName === "all"){
+            return setTagsSelected([]);
+        }
+        const alreadySelected = tagsSelected.includes(tagName);
+
+        if(alreadySelected){
+            const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+            setTagsSelected(filteredTags);
+        }
+        else{
+            setTagsSelected(prevState => [...prevState, tagName]);
+        }
+    }
+
+    function handleDetails(id){
+        navigate(`/details/${id}`);
+    }
+
+    useEffect(() => {
+        async function fetchTags(){
+            const response = await api.get("/tags");
+            setTags(response.data);
+        }
+
+        fetchTags();
+    }, []);
+
+    useEffect(() => {
+        async function fecthNotes(){
+            const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`);//nosso backend esta captando essas info via query
+            setNotes(response.data);
+        }
+
+        fecthNotes();
+
+    }, [tagsSelected, search]);//esse array diz sobre variaveis que se mudarem disparam o useeffect
+
     return(
         <Container>
             <Brand>
@@ -17,25 +66,47 @@ export function Home(){
 
             <Header/>
             <Menu>
-                <li><ButtonText title="Todos" isActive/></li>
-                <li><ButtonText title="React"/></li>
-                <li><ButtonText title="Nodejs"/></li>
+                <li>
+                    <ButtonText 
+                        title="Todos"
+                        onClick={() => handleTagSelected("all")}
+                        isActive={tagsSelected.length === 0}
+                    />
+                </li>
+
+                {
+                    tags && tags.map(tag =>
+                        <li key={String(tag.id)}>
+                            <ButtonText 
+                                title={tag.name}
+                                onClick={() => handleTagSelected(tag.name)}
+                                isActive={tagsSelected.includes(tag.name)}
+                            />
+                        </li>
+                    )
+                }
+                
             </Menu>
 
             <Search>
-                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+                <Input 
+                    placeholder="Pesquisar pelo título" 
+                    icon={FiSearch}
+                    onChange={e => setSearch(e.target.value)}
+                />
             </Search>
 
             <Content>
                 <Section title="Minhas notas">
-                    <Note data={{
-                        title: 'React Modal', 
-                        tags: [
-                            {id: '1', name: 'react'},
-                            {id: '2', name: 'rocketseat'}
-                        ]
-                    }}
-                    />
+                    {
+                        notes.map(note => (
+                            <Note 
+                                key={String(note.id)}
+                                data={note}
+                                onClick={() => handleDetails(note.id)}
+                            />
+                        ))
+                    }
                 </Section>
             </Content>
 
