2463dd8fcd411b703ad05118bd90ec3f999571a0
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-22 18:59:35 -0300

    Page details complete

--- ROCKETSEAT/Fase08/rocketnotes/src/pages/Details/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/pages/Details/index.jsx
@@ -4,46 +4,89 @@ import {Button} from "../../components/Button";
 import {Section} from "../../components/Section";
 import {Tag} from "../../components/Tag";
 import { ButtonText } from "../../components/ButtonText";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
 
 export function Details() {
+  const [data,setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  async function handleDeleteNote(){
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+    
+    if(confirm){
+      await api.delete(`/notes/${params.id}`);
+      navigate("/");
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote(){
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, []);
 
   return (
     <Container>
       <Header/>
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText 
+              title="Excluir nota"
+              onClick={handleDeleteNote}
+            />
 
-      <main>
-        <Content>
-          <ButtonText title="Hehehe"/>
+            <h1>
+              {data.title}
+            </h1>
 
-          <h1>
-            Introdução ao React
-          </h1>
+            <p>
+              {data.description}
+            </p>
+            { 
+              data.links && 
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={String(link.id)}>
+                        <a href={link.url} target="_blank">
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
 
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">Link 1</a>
-              </li>
-              <li>
-                <a href="#">Link 2</a>
-              </li>
-              <li>
-                <a href="#">Link 3</a>
-              </li>
-            </Links>
-          </Section>
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag
+                      key={String(tag.id)}
+                      title={tag.name}
+                    />
+                  ))
+                }
+              </Section>
+            }
 
-          <Section title="Marcadores">
-            <Tag title="vagabundo"></Tag>
-            <Tag title="chama"></Tag>
-          </Section>
-
-          <Button title="Voltar"/>
-        </Content>
-      </main>
+            <Button title="Voltar" to="/"/>
+          </Content>
+        </main>
+      }
     </Container>
 
   )
