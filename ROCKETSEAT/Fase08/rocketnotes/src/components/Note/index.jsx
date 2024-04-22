94146386a6606b2cae240a1228d8c25e1a3829f4
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-22 18:59:49 -0300

    Fixing props

--- ROCKETSEAT/Fase08/rocketnotes/src/components/Note/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/components/Note/index.jsx
@@ -1,9 +1,9 @@
-import {Container} from "./styles";7
+import {Container} from "./styles";
 import {Tag} from "../Tag";
 
 export function Note({data, ...rest}){
     return(
-        <Container>
+        <Container {...rest}>
             <h1>{data.title}</h1>
             {
                 data.tags &&
