a8f22dac79d88f9435a14f8086929fc885836c0c
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-22 18:57:39 -0300

    Fixing bug for routes

--- ROCKETSEAT/Fase08/rocketnotes/src/routes/index.jsx
+++ ROCKETSEAT/Fase08/rocketnotes/src/routes/index.jsx
@@ -1,14 +1,48 @@
-import { BrowserRouter } from 'react-router-dom';
-import { useAuth } from '../hooks/auth';
+import { BrowserRouter, Routes as RRDRoutes, Route, Navigate} from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 
-import { AppRoutes } from './app.routes';
-import { AuthRoutes } from './auth.routes';
+import { appRoutes } from "./app.routes";
+import {publicRoutes} from "./public.routes";
+
+const WithAuth = ({ children }) => {
+    const {user, isLoading} = useAuth();
+
+    if(isLoading){
+        return null
+    }
+
+    if(!user){
+        return <Navigate to={`/login?callbackUrl=${encodeURIComponent(window.location.pathname)}`} />
+    }
+
+    return children
+}
+
+const NoAuth = ({ children }) => {
+    const {user, isLoading} = useAuth();
+
+    if(isLoading){
+        return null
+    }
+
+    if(user){
+        return <Navigate to="/" />
+    }
+
+    return children
+}
 
 export function Routes(){
-    const {user} = useAuth();
     return(
         <BrowserRouter>
-            {user ? <AppRoutes/> : <AuthRoutes/>}
+            <RRDRoutes>
+                {publicRoutes.map(({ path, element }) => (
+                    <Route key={path} path={path} element={<NoAuth>{element}</NoAuth>} />
+                ))}
+                {appRoutes.map(({ path, element }) => (
+                    <Route key={path} path={path} element={<WithAuth>{element}</WithAuth>} />
+                ))}
+            </RRDRoutes>
         </BrowserRouter>
     );
 }
