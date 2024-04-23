8cb0b1e5003bd630350ebd51b5660aacb073c9d7
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-23 20:47:33 -0300

    Fixing query for get tags

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/controllers/MovieTagsController.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/controllers/MovieTagsController.js
@@ -2,10 +2,10 @@ const knex = require("../database/knex");
 
 class MovieTagsController{
     async index(req,res){
-        const user_id = req.user.id;
+        const {movie_id} = req.query;
 
         const tags = await knex("movie_tags")
-            .where({user_id})
+            .where({movie_id})
         
         return res.json(tags);
     }
