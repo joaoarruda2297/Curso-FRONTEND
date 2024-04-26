c54ad0dfdf022029e756507c837c954ea2a44235
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-26 19:31:19 -0300

    Linking with repositories

--- ROCKETSEAT/Fase07-09/API/src/controllers/UsersController.js
+++ ROCKETSEAT/Fase07-09/API/src/controllers/UsersController.js
@@ -1,6 +1,7 @@
 const {hash, compare} = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
+const UserRepository = require("../repositories/UserRepository");
 
 class UsersController{
     /* FORMA PADRÂO DE UM CONTROLLER
@@ -13,11 +14,9 @@ class UsersController{
 
     async create(request, response){
         const {name, email, password} = request.body;
+        const userRepository = new UserRepository();
 
-        const database = await sqliteConnection();
-        const checkUserExists = await database.get(
-            "SELECT * FROM users WHERE email = (?)", [email]
-        );
+        const checkUserExists = await userRepository.findByEmail(email);
 
         if(checkUserExists){
             throw new AppError("Este email já está em uso");
@@ -25,8 +24,8 @@ class UsersController{
 
         const hashedPassword = await hash(password, 8);
 
-        await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)",
-        [name,email,hashedPassword]);
+        userRepository.createUser(name, email, hashedPassword);
+
         return response.status(201).json({});
     }
 
