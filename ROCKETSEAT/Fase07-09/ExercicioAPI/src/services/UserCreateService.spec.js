3ece3f1be3f04fc150155bf418a4a9dc01f2f68a
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-29 22:53:27 -0300

    Tests for user create

---
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/services/UserCreateService.spec.js
@@ -0,0 +1,42 @@
+const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
+
+describe("UserCreateService", () => {
+    let userRepositoryInMemory = null;
+    let userCreateService = null;
+
+    beforeEach(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory();
+        userCreateService = new UserCreateService(userRepositoryInMemory);
+    });
+
+    it("user should be created", async () =>{
+        const user = {
+            name: 'User test',
+            email: "user@test.com",
+            password: "123"
+        };
+
+        const userCreated = await userCreateService.execute(user);
+    
+        expect(userCreated).toHaveProperty("id");
+    });
+
+    it("user email already on use", async () => {
+        const user1 = {
+            name: 'User test 1',
+            email: "user@test.com",
+            password: "123"
+        };
+
+        const user2 = {
+            name: 'User test 2',
+            email: "user@test.com",
+            password: "456"
+        };
+
+        await userCreateService.execute(user1);
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso.", 401));
+    });
+});
