f58f376aefae18d993746feae63d0734b3262316
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-29 22:54:11 -0300

    dependency inversion

---
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/services/UserCreateService.js
@@ -0,0 +1,24 @@
+const AppError = require("../utils/AppError");
+const {hash, compare} = require("bcrypt");
+
+class UserCreateService{
+    constructor(userRepository){
+        this.userRepository = userRepository;
+    }
+
+    async execute({name, email, password}){
+        const checkUserExist = await this.userRepository.findByEmail(email);        
+
+        if(checkUserExist){
+            throw new AppError("Este e-mail já está em uso.", 401);
+        }
+
+        const hashedPassword = await hash(password,8);//o metodo hash é assincrono, pois demora para criptografar, logo precisa de um await
+
+        const userCreated = await this.userRepository.createUser({name, email, password: hashedPassword});
+
+        return userCreated;
+    }
+}
+
+module.exports = UserCreateService;
