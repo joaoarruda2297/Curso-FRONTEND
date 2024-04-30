bfb22498704eb51febe01d32ecb7d42c30c490f0
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-04-29 22:53:45 -0300

    dependency inversion for db

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/repositories/UserRepository.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/repositories/UserRepository.js
@@ -1,5 +1,23 @@
+const sqliteConnection = require("../database/sqlite");
+
 class UserRepository{
     async findByEmail(email){
-        
+        const database = sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        return user;
+    }
+
+    async createUser({name, email, password}){
+        const database = sqliteConnection();
+
+        const userId = await database.run(
+            "INSERT INTO users (name,email,password) VALUES (?,?,?)", 
+            [name, email, password]
+        );
+
+        return {id: userId};
     }
 }
+
+module.exports = UserRepository;
