ed1b28cfe5700335908817ab014358fabd0a514b
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-05-01 23:43:53 -0300

    Changing function for update user to a service file

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/controllers/UsersController.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/controllers/UsersController.js
@@ -4,6 +4,7 @@ const {hash, compare} = require("bcrypt");
 const sqliteConnection = require("../database/sqlite");
 const UserRepository = require("../repositories/UserRepository");
 const UserCreateService = require("../services/UserCreateService");
+const UserUpdateService = require("../services/UserUpdateService");
 
 class UsersController{
     /* FORMA PADRÂO DE UM CONTROLLER
@@ -22,53 +23,16 @@ class UsersController{
         await userCreateService.execute({name, email, password});
 
         return res.status(201).json();
-
     }
 
     async update(req, res){
         const {name, email, password, old_password} = req.body;
         const user_id = req.user.id;
 
-        const database = await sqliteConnection();
-        const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
+        const userRepository = new UserRepository();
+        const userUpdateService = new UserUpdateService(userRepository);
 
-        //verificando se o usuario realmente existe
-        if(!user){
-            throw new AppError("Usuário não encontrado");
-        }
-
-        //verificando se está tentando trocar para um email que já está em uso
-        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
-        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
-            throw new AppError("Este email já está em uso!");
-        }
-
-        user.name = name ?? user.name;
-        user.email = email ?? user.email;
-
-        if(password && !old_password){
-            throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
-        }
-
-        if(password && old_password){
-            const checkOldPassword = await compare(old_password, user.password);
-
-            if(!checkOldPassword){
-                throw new AppError("Senha antiga não confere");
-            }
-
-            user.password = await hash(password, 8);
-        }
-
-        await database.run(`
-            UPDATE users SET
-            name = ?,
-            email = ?,
-            password = ?,
-            updated_at = DATETIME('now')
-            WHERE id = ?`,
-            [user.name, user.email,user.password, user_id]
-        );
+        await userUpdateService.execute({name, email, password, old_password, user_id});
 
         return res.status(200).json();
     }
