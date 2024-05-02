ffd6ae3f2a8ffa7ffd5ff4c548c5729d88d49b77
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-05-01 23:44:54 -0300

    Creating functions for update user and find by id for tests repository

--- ROCKETSEAT/Fase07-09/ExercicioAPI/src/repositories/UserRepositoryInMemory.js
+++ ROCKETSEAT/Fase07-09/ExercicioAPI/src/repositories/UserRepositoryInMemory.js
@@ -17,6 +17,27 @@ class UserRepositoryInMemory{
     async findByEmail(email){
         return this.users.find(user => user.email === email);
     }
+
+    async findByUserId({user_id}){
+        return this.users.find(user => user.id === user_id);
+    }
+
+    async updateUser({name, email, password, user_id}){
+        const user = {
+            id: user_id,
+            email: email,
+            name: name,
+            password: password
+        };
+
+        this.users.find(useri => {
+            if(useri.id === user.id){
+                useri = user;
+            }
+        });
+
+        return user;
+    }
 }
 
 module.exports = UserRepositoryInMemory;
