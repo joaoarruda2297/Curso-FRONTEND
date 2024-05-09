f137b53095b7960d51a1e980f18682f9e892e8b7
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-05-08 22:00:42 -0300

    Users validated controller

---
+++ ROCKETSEAT/Fase10/api/src/controllers/UsersValidatedController.js
@@ -0,0 +1,19 @@
+const knex = require("../database/knex");
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+
+class UsersValidatedController {
+  async index(request, response) {
+    const { user } = request;
+
+    const checkUserExists = await knex("users").where({ id: user.id });
+
+    if (checkUserExists.length > 0) {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    return response.status(201).json();
+  }
+}
+
+module.exports = UsersValidatedController;
