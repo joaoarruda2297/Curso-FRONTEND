30ec310492d62d77c991040b471dd58fdcd50bd3
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-05-15 19:58:39 -0300

    Responsive shortTask

--- ROCKETSEAT/Fase10/e-tasks/src/components/ShortTask/styles.js
+++ ROCKETSEAT/Fase10/e-tasks/src/components/ShortTask/styles.js
@@ -3,18 +3,20 @@ import styled from 'styled-components';
 export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   color: ${({ theme }) => theme.COLORS.GRAY_100};
-  height: 44px;
+  height: 2.75rem;
   border: 0;
-  padding: 0 16px;
-  border-radius: 7px;
-  font-size: 14px;
+  padding: 0 1rem;
+  border-radius: 0.4375rem;
+  font-size: 0.875rem;
   
   display: flex;
   align-items: center;
-  gap: 7px;
+  gap: 0.4375rem;
 
   > svg {
-    font-size: 18px;
+    font-size: 1.125rem;
     color: ${({ theme }) => theme.COLORS.RED_200};
   }
+
+
 `;
