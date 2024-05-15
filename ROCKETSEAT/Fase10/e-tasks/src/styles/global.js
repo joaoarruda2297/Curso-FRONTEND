f5aca80a8432f728f009453f56efd2ff461b9d5a
joaoarruda2297 <jovitorarruda@hotmail.com>
2024-05-14 21:06:51 -0300

    Adding rem for font-size

--- ROCKETSEAT/Fase10/e-tasks/src/styles/global.js
+++ ROCKETSEAT/Fase10/e-tasks/src/styles/global.js
@@ -7,6 +7,10 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root{
+    font-size: 62.5%;
+  }
+
   body {
     background-color: ${({ theme }) => theme.COLORS.GRAY_800};
     color: ${({ theme }) => theme.COLORS.GRAY_100};
@@ -16,7 +20,7 @@ export default createGlobalStyle`
 
   body, input, button, textarea {
     font-family: 'Roboto', sans-serif;
-    font-size: 16px;
+    font-size: 1.6rem;
     outline: none;
   }
 
